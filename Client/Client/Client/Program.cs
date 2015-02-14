251f05c2de6b0d9f692a53a9cd36a2fe7fe00357
Geo <geo_ronaldo_94@yahoo.com>
2015-02-14 12:57:10 +0200

    Add Client and Server project files

---
+++ Client/Client/Client/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClientTest
+{
+    public class SynchronousSocketClient
+    {
+
+        static IPHostEntry ipHostInfo;
+        static IPAddress ipAddress;
+        static IPEndPoint remoteEP;
+        static Socket sender;
+        static byte[] bytes;
+
+        private static void Initialize()
+        {
+            remoteEP = new IPEndPoint(IPAddress.Parse("192.168.0.113"), 11000);
+            sender = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            bytes = new byte[1024];
+        }
+
+        public static void StartClient(Message message)
+        {
+            try
+            {
+                Initialize();
+
+                try
+                {
+                    sender.Connect(remoteEP);
+
+                    Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
+
+                    byte[] msg = message.GetByteMessage();
+                    int bytesSent = sender.Send(msg);
+                    int bytesRec = sender.Receive(bytes);
+
+                    Console.WriteLine("Echo test = {0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public class Message
+        {
+            public string MessageText;
+            public string IP;
+            public static string SEND_MESSAGE;
+
+            public Message(string ip)
+            {
+                Console.WriteLine("Type your message:");
+                MessageText = Console.ReadLine();
+                IP = ip;
+                Console.WriteLine("Send meesage (y/n):");
+                SEND_MESSAGE = Console.ReadLine();
+            }
+
+            public byte[] GetByteMessage()
+            {
+                return Encoding.ASCII.GetBytes(MessageText);
+            }
+
+        }
+
+        public static int Main(String[] args)
+        {
+
+            do
+            {
+                StartClient(new Message("192.168.0.113"));
+            } while (Message.SEND_MESSAGE != "n");
+
+            return 0;
+        }
+    }
+}
---
+++ Server/Server/Server/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerTest
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+
+    public class SynchronousSocketListener
+    {
+
+        public static string data = null;
+
+        public static IPHostEntry ipHostInfo;
+        public static IPAddress ipAddress;
+        public static IPEndPoint localEndPoint;
+        public static byte[] bytes;
+
+        private static void Initialize()
+        {
+            ipHostInfo = Dns.Resolve(Dns.GetHostName());
+            ipAddress = ipHostInfo.AddressList[0];
+            localEndPoint = new IPEndPoint(ipAddress, 11000);
+            bytes = new Byte[1024];
+        }
+
+        public static void StartListening()
+        {
+            Initialize();
+
+            Socket listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+                listener.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting for a connection...");
+                    Socket handler = listener.Accept();
+                    data = null;
+
+                    while (true)
+                    {
+                        bytes = new byte[1024];
+                        int bytesRec = handler.Receive(bytes);
+                        data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                        break;
+                    }
+
+                    Console.WriteLine("Text received : {0}", data);
+
+                    Console.WriteLine("Type your message:");
+                    string message = Console.ReadLine();
+
+                    byte[] msg = Encoding.ASCII.GetBytes(data + "\n" + message);
+
+                    handler.Send(msg);
+                    handler.Shutdown(SocketShutdown.Both);
+                    handler.Close();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("\nPress ENTER to continue...");
+            Console.Read();
+
+        }
+
+
+        public static int Main(String[] args)
+        {
+            StartListening();
+            return 0;
+        }
+    }
+
+}
